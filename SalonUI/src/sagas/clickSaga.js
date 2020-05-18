f5e323f1cb1f4d042212d7c789e0c1190f88be5b
AnastasiyaKislach <anastasiya.kislach@gmail.com>
2020-05-18 12:57:34 +0300

    UI
    Added redux-saga

---
+++ SalonUI/src/sagas/clickSaga.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime/runtime';
+
+function* clickSaga() {
+  // eslint-disable-next-line no-console
+  console.log('Example saga reached');
+  yield;
+}
+
+export default clickSaga;
--- SalonUI/src/store/configureStore.js
+++ SalonUI/src/store/configureStore.js
@@ -1,7 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { clickReducer } from '../reducers/clickReducer';
+import clickSaga from '../sagas/clickSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export function configureStore() {
-  let store = createStore(clickReducer);
+  let store = createStore(clickReducer, applyMiddleware(sagaMiddleware));
+  sagaMiddleware.run(clickSaga);
   return store;
 }
